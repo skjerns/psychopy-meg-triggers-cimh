f33162937001dee412562180579e098ebd742d6d
Simon Kern <14980558+skjerns@users.noreply.github.com>
2023-11-16 15:44:13 +0100

    Update __init__.py

--- meg_triggers/__init__.py
+++ meg_triggers/__init__.py
@@ -8,9 +8,6 @@ import traceback
 from queue import Empty, Queue
 
 import numpy as np
-# Part of the PsychoPy library
-# Copyright (C) 2002-2018 Jonathan Peirce (C) 2019-2022 Open Science Tools Ltd.
-# Distributed under the terms of the GNU General Public License (GPL).
 from psychopy import core
 
 
@@ -62,11 +59,13 @@ def int_to_binary(number):
     assert 0<=number<256, f'trigger value needs to be between 0 and 255, but {number=}'
     return np.array([x for x in bin(number)[2:].zfill(8)], dtype=np.uint8)
 
-class MEGTriggerThread(threading.Thread):
+class _MEGTriggerThread(threading.Thread):
     def __init__(self, q):
-        super(MEGTriggerThread,self).__init__(daemon=True)
+        super(_MEGTriggerThread,self).__init__(daemon=True)
         self.q = q
         self.keep_running = True
+        self.default_duration = None
+        self.default_reset_value = 0
         self.verbose = False
         return
 
@@ -116,28 +115,24 @@ class MEGTriggerThread(threading.Thread):
         tpydaqmxtask.WriteDigitalLines(1,1,10.0,PyDAQmx.DAQmx_Val_GroupByChannel,
                                        value_bin,None,None)
 
-    def send_trigger(self, value_bin, duration=0):
+    def send_trigger(self, value_bin, duration=None, reset_value=None):
         start_time = time.perf_counter()
         self._send_trigger(value_bin)
 
         if self.verbose or ENABLE_DEBUG:
-            msg = f'set trigger channel to {value_bin} @{core.getTime():.3f}s for '
-            if duration>0:
-                msg += f'{duration:.3f}s'
-            else:
-                msg += ' inf s'
+            msg = f'set trigger channel to {value_bin} @{core.getTime():.3f}s '
             _print(msg)
 
-        if duration>0:
+        if duration is not None:
             core.wait(duration)
-            self._send_trigger(0)
+            self._send_trigger(reset_value)
             duration = time.perf_counter() - start_time
             _print(f'reset trigger channel to 0, was active for {duration:.3f}s')
 
 #########################
 
 _queue = Queue()
-_meg_trigger_thread = MEGTriggerThread(q=_queue)
+_meg_trigger_thread = _MEGTriggerThread(q=_queue)
 _meg_trigger_thread.start()
 
 @atexit.register
@@ -148,6 +143,17 @@ def _atexit():
     _meg_trigger_thread.disconnect()
     _meg_trigger_thread.join(timeout=0.25)
 
+def set_default_duration(duration):
+    """set default duration that is used if no duration is indicated"""
+    if duration>0.1: 
+        _print(f'default duration is set to {duration} seconds, seems a bit long?')
+    _meg_trigger_thread.default_duration = duration
+
+def set_default_resetvalue(reset_value):
+    """this value is used to reset the channel to a specific value after 
+    a call with duration>0"""
+    _meg_trigger_thread.default_reset_value = reset_value
+
 
 def enable_printing():
     """print the send trigger values to the console"""
@@ -157,7 +163,7 @@ def disable_printing():
     """dont print anything. if ENABLE_DEBUG is active, will always print."""
     _meg_trigger_thread.verbose = False
 
-def send_trigger(value, duration=0):
+def send_trigger(value, duration=None, reset_value=None):
     """send a trigger to the NI6321 device
     :param value:     can be any int between 0 and 255
     :param duration:  send
@@ -170,4 +176,11 @@ def send_trigger(value, duration=0):
     else:
         raise ValueError('trigger value must be array, tuple or list')
     assert _meg_trigger_thread.is_alive(), 'ERROR: trigger thread died. Please report this error!'
-    _queue.put_nowait([value_bin, duration])
+    
+    if duration is None:
+        duration = _meg_trigger_thread.default_duration
+    
+    if reset_value is None:
+        duration = _meg_trigger_thread.default_reset_value
+    
+    _queue.put_nowait([value_bin, duration, reset_value])
